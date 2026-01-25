a9747a1745f364af3d24ea576f33d13a4bb634f4
Alejandro Moreno <alejandromogil@gmail.com>
2026-01-25 08:33:40 -0500

    core: add ApiOperation decorators for chatbot response and emotion analysis endpoints

--- src/modules/chatbot/presentation/controllers/chatbot.controller.ts
+++ src/modules/chatbot/presentation/controllers/chatbot.controller.ts
@@ -3,6 +3,7 @@ import { GetChatbotResponseDto } from '../dto/requests/conversation-request.dto'
 import { GetChatbotResponseUseCase } from '@modules/chatbot/application/use-cases/get-chatbot-response.usecase';
 import { GetChatbotAnalysisDto } from '../dto/requests/analysis-request.dto';
 import { GetChatbotAnalysisUseCase } from '@modules/chatbot/application/use-cases/get-chatbot-analysis.usecase';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('chatbot')
 export class ChatbotController {
@@ -11,11 +12,13 @@ export class ChatbotController {
     private readonly getChatbotAnalysisUseCase: GetChatbotAnalysisUseCase,
   ) {}
 
+  @ApiOperation({ summary: 'Get Chatbot Response' })
   @Post('response')
   getResponse(@Body() dto: GetChatbotResponseDto) {
     return this.getChatbotResponseUseCase.execute(dto);
   }
 
+  @ApiOperation({ summary: 'Get Emotion Analysis' })
   @Post('emotion-analysis')
   getEmotionAnalysis(@Body() dto: GetChatbotAnalysisDto) {
     return this.getChatbotAnalysisUseCase.execute(dto);

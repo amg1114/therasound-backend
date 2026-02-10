ef94d6ad2fe0b27f6cc5675cc91e58ce457e0537
Alejandro Moreno <alejandromogil@gmail.com>
2026-02-09 21:04:22 -0500

    feat: implement GetCurrentUserUseCase to return user profile with preferences and recent playlists; add ProfileResponseDto

---
+++ src/modules/auth/presentation/dto/responses/profile-response.dto.ts
@@ -0,0 +1,18 @@
+import { UserResponseDto } from '@modules/users/presentation/dto/responses/user-response.dto';
+import { UserPreferencesResponseDto } from '@modules/users/presentation/dto/responses/user-preferences-response.dto';
+import { PlaylistSummaryResponseDto } from '@modules/playlists/presentation/dto/responses/playlist-summary-response.dto';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ProfileResponseDto {
+  @ApiProperty({ type: () => UserResponseDto })
+  user: UserResponseDto;
+
+  @ApiProperty({ type: () => UserPreferencesResponseDto })
+  userPreferences: UserPreferencesResponseDto;
+
+  @ApiProperty({
+    type: () => [PlaylistSummaryResponseDto],
+    description: 'Últimas 5 listas de reproducción del usuario',
+  })
+  recentPlaylists: PlaylistSummaryResponseDto[];
+}

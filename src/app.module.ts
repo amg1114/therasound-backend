08706781202ebed2c9061e4847a3f927025ae37b
Alejandro Moreno <alejandromogil@gmail.com>
2026-02-14 10:34:00 -0500

    feat: restructure app configuration; move to separate config files and add emotion weights

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { APP_CONFIG_SCHEMA, appConfig } from './app.config';
-import { ChatbotModule } from './modules/chatbot/chatbot.module';
-import { SongsModule } from './modules/songs/songs.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './modules/users/users.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { PlaylistsModule } from './modules/playlists/playlists.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { GenresModule } from './modules/genres/genres.module';
-import { AdminModule } from './modules/admin/admin.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { APP_CONFIG_SCHEMA, appConfig } from './config/app.config';
+import { AdminModule } from './modules/admin/admin.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ChatbotModule } from './modules/chatbot/chatbot.module';
+import { GenresModule } from './modules/genres/genres.module';
+import { PlaylistsModule } from './modules/playlists/playlists.module';
+import { SongsModule } from './modules/songs/songs.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [

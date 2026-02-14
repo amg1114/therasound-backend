057e9ef5ffc1e1fefa5cac6cdee31851320e02c4
Alejandro Moreno <alejandromogil@gmail.com>
2026-02-14 10:34:00 -0500

    feat: restructure app configuration; move to separate config files and add emotion weights

---
+++ src/config/app.config.ts
@@ -0,0 +1,75 @@
+import { AudioFeaturesVO } from '@modules/songs/domain/value-objects/audio-features.vo';
+import { SongEmotionType } from '@modules/songs/domain/value-objects/song-emotion.vo';
+import { readFileSync } from 'fs';
+import * as Joi from 'joi';
+import { join } from 'path';
+
+export type AppConfig = {
+  app: {
+    env: string;
+    port: number;
+  };
+  openrouter: {
+    apiKey: string;
+  };
+  database: {
+    uri: string;
+    name: string;
+  };
+  jwt: {
+    secret: string;
+  };
+  soundcharts: {
+    appId: string;
+    apiKey: string;
+  };
+  emotionAnalysis: {
+    apiUrl: string;
+  };
+  emotionWeights: Record<SongEmotionType, AudioFeaturesVO>;
+};
+
+const emotionWeightsPath = join(
+  process.cwd(),
+  'src/config',
+  'feature_emotion_weights.json',
+);
+const emotionWeights = JSON.parse(
+  readFileSync(emotionWeightsPath, 'utf-8'),
+) as Record<SongEmotionType, AudioFeaturesVO>;
+
+export const APP_CONFIG_SCHEMA = Joi.object({
+  PORT: Joi.number().default(3000),
+  OPENROUTER_API_KEY: Joi.string().required(),
+  DATABASE_URI: Joi.string().required(),
+  DATABASE_NAME: Joi.string().required(),
+  JWT_SECRET: Joi.string().default('default_jwt_secret'),
+  SOUNDCHARTS_APP_ID: Joi.string().required(),
+  SOUNDCHARTS_API_KEY: Joi.string().required(),
+  EMOTION_ANALYSIS_API_URL: Joi.string().default('http://localhost:8000'),
+});
+
+export const appConfig = (): AppConfig => ({
+  app: {
+    env: process.env.NODE_ENV || 'dev',
+    port: parseInt(process.env.PORT || '3000', 10),
+  },
+  openrouter: {
+    apiKey: process.env.OPENROUTER_API_KEY!,
+  },
+  database: {
+    uri: process.env.DATABASE_URI!,
+    name: process.env.DATABASE_NAME!,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'default_jwt_secret',
+  },
+  soundcharts: {
+    appId: process.env.SOUNDCHARTS_APP_ID!,
+    apiKey: process.env.SOUNDCHARTS_API_KEY!,
+  },
+  emotionAnalysis: {
+    apiUrl: process.env.EMOTION_ANALYSIS_API_URL || 'http://localhost:8000',
+  },
+  emotionWeights,
+});

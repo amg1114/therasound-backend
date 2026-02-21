a445e05d69f06483a414ff4430333ed4a7d420df
Alejandro Moreno <alejandromogil@gmail.com>
2026-02-20 21:12:16 -0500

    feat: remove GetChatbotAnalysisUseCase and related references from Chatbot module and controller

--- src/modules/chatbot/presentation/controllers/chatbot.controller.ts
+++ src/modules/chatbot/presentation/controllers/chatbot.controller.ts
@@ -1,27 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { GetChatbotResponseDto } from '../dto/requests/conversation-request.dto';
 import { GetChatbotResponseUseCase } from '@modules/chatbot/application/use-cases/get-chatbot-response.usecase';
-import { GetChatbotAnalysisDto } from '../dto/requests/analysis-request.dto';
-import { GetChatbotAnalysisUseCase } from '@modules/chatbot/application/use-cases/get-chatbot-analysis.usecase';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { GetChatbotResponseDto } from '../dto/requests/conversation-request.dto';
 
 @Controller('chatbot')
 @ApiBearerAuth()
 export class ChatbotController {
   constructor(
     private readonly getChatbotResponseUseCase: GetChatbotResponseUseCase,
-    private readonly getChatbotAnalysisUseCase: GetChatbotAnalysisUseCase,
   ) {}
 
   @ApiOperation({ summary: 'Get Chatbot Response' })
   @Post('response')
   getResponse(@Body() dto: GetChatbotResponseDto) {
     return this.getChatbotResponseUseCase.execute(dto);
-  }
-
-  @ApiOperation({ summary: 'Get Emotion Analysis' })
-  @Post('emotion-analysis')
-  getEmotionAnalysis(@Body() dto: GetChatbotAnalysisDto) {
-    return this.getChatbotAnalysisUseCase.execute(dto);
   }
 }

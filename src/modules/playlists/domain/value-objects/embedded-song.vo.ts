2b8dec76077ea42134404c47bbd5fa81f3c63cd4
Alejandro Moreno <alejandromogil@gmail.com>
2026-02-22 11:30:13 -0500

    feat: add emotionProbabilities field to EmbeddedSongVO type

--- src/modules/playlists/domain/value-objects/embedded-song.vo.ts
+++ src/modules/playlists/domain/value-objects/embedded-song.vo.ts
@@ -1,9 +1,12 @@
+import { EmotionProbabilitiesVO } from '@modules/songs/domain/value-objects/emotion-probabilities.vo';
+
 export type EmbeddedSongVO = {
   id: string;
   spotifyId: string;
   title: string;
   artist: string;
   emotion: string;
+  emotionProbabilities: EmotionProbabilitiesVO;
   durationMs: number;
   spotifyUrl: string;
   genres: string[];

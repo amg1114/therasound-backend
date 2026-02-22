d0aba4e8d7a48fa8d63e58aefb117348232baf88
Alejandro Moreno <alejandromogil@gmail.com>
2026-02-22 13:45:17 -0500

    feat: implement groupByDominantEmotion method in SongEntity for emotion-based grouping

--- src/modules/songs/domain/entities/song.entity.ts
+++ src/modules/songs/domain/entities/song.entity.ts
@@ -1,8 +1,8 @@
+import { EmotionType } from '@common/domain/value-objects/emotion.vo';
 import { EmotionVO } from '../../../../common/domain/value-objects/emotion.vo';
 import { AudioFeaturesVO } from '../value-objects/audio-features.vo';
 import { EmotionDistancesVO } from '../value-objects/emotion-distances.vo';
 import { EmotionProbabilitiesVO } from '../value-objects/emotion-probabilities.vo';
-
 export class SongEntity {
   id: string;
   spotifyId: string;
@@ -34,6 +34,29 @@ export class SongEntity {
     return song;
   }
 
+  // En SongEntity
+  static groupByDominantEmotion(
+    songs: SongEntity[],
+  ): Record<string, SongEntity[]> {
+    return EmotionVO.SONG_EMOTIONS.reduce(
+      (acc, emotion) => {
+        acc[emotion] = songs
+          .filter((s) => {
+            const minDistance = Math.min(
+              ...EmotionVO.SONG_EMOTIONS.map((e) => s.getEmotionDistance(e)),
+            );
+            return s.getEmotionDistance(emotion) === minDistance;
+          })
+          .sort(
+            (a, b) =>
+              b.getEmotionDistance(emotion) - a.getEmotionDistance(emotion),
+          );
+        return acc;
+      },
+      {} as Record<string, SongEntity[]>,
+    );
+  }
+
   updateEmotionAnalysis(
     emotion: EmotionVO,
     confidence: number,
@@ -50,5 +73,14 @@ export class SongEntity {
     if (transitionScoring) {
       this.emotionDistances = transitionScoring;
     }
+  }
+
+  getEmotionDistance(targetEmotion: EmotionVO | EmotionType): number {
+    const targetEmotionValue =
+      targetEmotion instanceof EmotionVO
+        ? targetEmotion.getValue()
+        : targetEmotion;
+
+    return this.emotionDistances[targetEmotionValue];
   }
 }

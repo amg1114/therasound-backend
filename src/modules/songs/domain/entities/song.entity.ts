54ff64d4cd636eaadc3a64081fce831273f89e7d
Alejandro Moreno <alejandromogil@gmail.com>
2026-02-22 14:52:51 -0500

    feat: refactor emotion analysis in SongEntity and related DTOs to streamline data handling

--- src/modules/songs/domain/entities/song.entity.ts
+++ src/modules/songs/domain/entities/song.entity.ts
@@ -5,11 +5,15 @@ import { EmotionDistancesVO } from '../value-objects/emotion-distances.vo';
 import { EmotionProbabilitiesVO } from '../value-objects/emotion-probabilities.vo';
 export class SongEntity {
   id: string;
+
+  // External metadata
+  reccobeatsId?: string;
   spotifyId: string;
+  spotifyUrl: string;
+
   title: string;
   artist: string;
   durationMs: number;
-  spotifyUrl: string;
   genres: string[];
   imageUrl: string;
   releaseDate: Date;
@@ -17,9 +21,7 @@ export class SongEntity {
 
   // Emotion analysis data
   emotion: EmotionVO;
-  emotionConfidence: number;
   emotionProbabilities: EmotionProbabilitiesVO;
-  reccobeatsId?: string;
   emotionDistances: EmotionDistancesVO;
 
   // Statistics
@@ -59,20 +61,12 @@ export class SongEntity {
 
   updateEmotionAnalysis(
     emotion: EmotionVO,
-    confidence: number,
-    probabilities: EmotionProbabilitiesVO,
-    reccobeatsId?: string,
-    transitionScoring?: EmotionDistancesVO,
+    emotionProbabilities: EmotionProbabilitiesVO,
+    emotionDistances: EmotionDistancesVO,
   ) {
     this.emotion = emotion;
-    this.emotionConfidence = confidence;
-    this.emotionProbabilities = probabilities;
-    if (reccobeatsId) {
-      this.reccobeatsId = reccobeatsId;
-    }
-    if (transitionScoring) {
-      this.emotionDistances = transitionScoring;
-    }
+    this.emotionProbabilities = emotionProbabilities;
+    this.emotionDistances = emotionDistances;
   }
 
   getEmotionDistance(targetEmotion: EmotionVO | EmotionType): number {

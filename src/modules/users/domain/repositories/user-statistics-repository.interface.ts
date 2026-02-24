df9ab9b1a3f0549e26b4bff55a3e6320256ed0c6
Alejandro Moreno <alejandromogil@gmail.com>
2026-02-24 18:07:34 -0500

    feat(users): update create method to accept Partial<UserStatisticsEntity> in repository and mapper

--- src/modules/users/domain/repositories/user-statistics-repository.interface.ts
+++ src/modules/users/domain/repositories/user-statistics-repository.interface.ts
@@ -3,7 +3,9 @@ import { UserStatisticsEntity } from '../entities';
 export const USER_STATISTICS_REPOSITORY = 'USER_STATISTICS_REPOSITORY';
 
 export interface IUserStatisticsRepository {
-  create(userStatistics: UserStatisticsEntity): Promise<UserStatisticsEntity>;
+  create(
+    userStatistics: Partial<UserStatisticsEntity>,
+  ): Promise<UserStatisticsEntity>;
 
   findById(id: string): Promise<UserStatisticsEntity | null>;
 

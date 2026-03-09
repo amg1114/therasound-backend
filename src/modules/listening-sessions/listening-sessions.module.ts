f8e4bdac794317805d685de8b6349743871d2d8c
Alejandro Moreno <alejandromogil@gmail.com>
2026-03-08 20:48:45 -0500

    feat(listening-sessions): implement MongoListeningSessionRepository and update ListeningSessionMapper for entity handling

--- src/modules/listening-sessions/listening-sessions.module.ts
+++ src/modules/listening-sessions/listening-sessions.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LISTENING_SESSION_REPOSITORY } from './domain/repositories/listening-session.repository.interface';
 import {
   ListeningSessionSchema,
   MongoListeningSessionEntity,
 } from './infrastructure/mongo/entities';
+import { MongoListeningSessionRepository } from './infrastructure/mongo/repositories';
 
 @Module({
   imports: [
@@ -14,5 +16,12 @@ import {
       },
     ]),
   ],
+  providers: [
+    {
+      provide: LISTENING_SESSION_REPOSITORY,
+      useClass: MongoListeningSessionRepository,
+    },
+  ],
+  exports: [LISTENING_SESSION_REPOSITORY],
 })
 export class ListeningSessionsModule {}
